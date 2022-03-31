aa033c5e82af129d4d484ab34f852d0ec739ac11
Ten Valeriya <kogaisvetlana@gmail.com>
2022-03-31 15:58:03 +0300

    fix bug with read data cencer from text file

--- NotLinearCancerModel/ActionDataFile.cs
+++ NotLinearCancerModel/ActionDataFile.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Globalization;
 
 namespace NotLinearCancerModel
 {
@@ -16,7 +17,8 @@ namespace NotLinearCancerModel
         {
             string message = "Ok";
             pathToSave += pathToSave + type + @"\txt\" + number.ToString() + type + @".txt";
-            try {
+            try
+            {
                 StreamWriter outputFile = new StreamWriter(pathToSave);
                 for (int i = 0; i <= data.GetLength(0); i++)
                 {
@@ -51,6 +53,7 @@ namespace NotLinearCancerModel
 
         static public List<List<float>> getDataFromFile(string type, int number, string pathToFile = @"D:\VolSU\НИР\ScienceArticle\NotLinearCancerModel\NotLinearCancerModel\dataTumor\ModelData\personalPatients\poly3current\")
         {
+            number++;
             List<float> xValues = new List<float>();
             List<float> yValues = new List<float>();
             List<List<float>> xyValues = new List<List<float>>()
@@ -61,15 +64,15 @@ namespace NotLinearCancerModel
             };
             int counter = 0;
             pathToFile = pathToFile + type + @"\txt\" + number.ToString() + type + @".txt";
+            Console.WriteLine(pathToFile);
             // Read the file and display it line by line.  
             try
             {
                 foreach (string line in System.IO.File.ReadLines(pathToFile))
                 {
-                    /*loat x = float.Parse(line.Split(" ")[0].Trim());
-                    float y = float.Parse(line.Split(" ")[1].Trim());*/
-                    xyValues[0].Add(float.Parse(line.Split(" ")[0].Trim()));
-                    xyValues[0].Add(float.Parse(line.Split(" ")[1].Trim()));
+                string[] valuesSingleXY = line.Split("\t");System.Diagnostics.Debug.WriteLine(valuesSingleXY[0]);
+                xyValues[0].Add(float.Parse(valuesSingleXY[0].Trim(), CultureInfo.InvariantCulture));
+                    xyValues[0].Add(float.Parse(valuesSingleXY[1].Trim(), CultureInfo.InvariantCulture));
                     counter++;
                 }
             }
@@ -85,7 +88,7 @@ namespace NotLinearCancerModel
             {
                 Console.WriteLine($"The file could not be opened: '{e}'");
             }
-            
+
             return xyValues;
         }
     }
