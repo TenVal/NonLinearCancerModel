4c4d008a484ba7bd49f8a8df39ffa90a37ca00f9
Ten Valeriya <kogaisvetlana@gmail.com>
2022-04-26 02:18:19 +0300

    add implementation for calculate any cancer and folders fot it, try fix bugs in "find min" HomeView and add every parameters about diffusion method

--- NotLinearCancerModel/MVVM/View/CalculateOneView.xaml.cs
+++ NotLinearCancerModel/MVVM/View/CalculateOneView.xaml.cs
@@ -27,6 +27,42 @@ namespace NotLinearCancerModel.MVVM.View
 
         private void Calculate_Click(object sender, RoutedEventArgs e)
         {
+            float length = float.Parse(TextBoxLength.Text);
+            float RightX = length;
+            float h = float.Parse(TextBoxH.Text);
+            float d = float.Parse(TextBoxD.Text);
+            float k = float.Parse(TextBoxK.Text);
+            float speed = float.Parse(TextBoxSpeed.Text);
+            float angleXY = float.Parse(TextBoxAngleXY.Text);
+            float angleZ = float.Parse(TextBoxAngleZ.Text);
+            float tMax = float.Parse(TextBoxTMax.Text);
+
+            C c = new C(speed, angleXY, angleZ);
+            Q q = new Q(0);
+            D dF = new D(speed, d);
+
+            MethodDiffusion diffusion = new MethodDiffusion(dF, c, q);
+
+            for (int i = 0; i < 10; i++)
+            {
+                int N = (int)(length / h);
+                double[,,] valuesP = new double[N, N, N];
+                diffusion.getValues(tMax, h, k, length, valuesP);
+
+                float[] numberPointsVolume = new float[diffusion.NumberPointsVolume.Count];
+                Array.Copy(diffusion.NumberPointsVolume.ToArray(), numberPointsVolume, numberPointsVolume.Length);
+                float[] tValues = new float[diffusion.TValues.Count];
+                Array.Copy(diffusion.TValues.ToArray(), tValues, tValues.Length);
+
+                // write every data about modeling to files
+                ActionDataFile.writeDataToFile("Volume", i, valuesP);
+                // Write time-value data to file
+                ActionDataFile.writeTimeValueToFile("Volume", i, tValues, numberPointsVolume);
+                // write params of modeling to file
+                float[] paramsForCancer = { speed, d, k };
+                ActionDataFile.writeParametersToFile(type: "Volume", number: i, cancerParameters: paramsForCancer);
+            }
+
             MessageBox.Show("UUUUUUURRRRAAAAAAA");
         }
     }
