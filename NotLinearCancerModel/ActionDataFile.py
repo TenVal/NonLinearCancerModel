9b25032665c55c385a658805223f33d06d2e2975
Ten Valeriya <kogaisvetlana@gmail.com>
2022-04-29 01:31:32 +0300

    change in output data (img) to see it over a whole period of time

--- NotLinearCancerModel/ActionDataFile.py
+++ NotLinearCancerModel/ActionDataFile.py
@@ -1,19 +1,19 @@
 
-def writeTimeValueIntoFile(type, number, timeValue):
-
-    with open(f"PredictData/PersonalPatients/{type}/timeValue/txt/{number}{type}.txt", "w") as file:
+def writeTimeValueIntoFile(type, number, timeValue, path = "PredictData/PersonalPatients/"):
+    path = path + str(type) + "/timeValue/txt/" + str(number) + str(type) + ".txt"
+    with open(path, "w") as file:
         for i in range(len(timeValue[0])):
             file.write(f"{timeValue[0][i]}\t{timeValue[1][i]}\n".encode('utf-8').decode('utf-8'))
 
 
-def writeDataIntoFile(type, number, xyzc):
-
-    with open(f"PredictData/PersonalPatients/{type}/txt/{number}{type}.txt", "w") as file:
+def writeDataIntoFile(type, number, xyzc, path = "PredictData/PersonalPatients/"):
+    path = "PredictData/PersonalPatients/" + str(type) + "/txt/" + str(number) + str(type) + ".txt"
+    with open(path, "w") as file:
         for i in range(len(xyzc[0])):
             file.write(f"{xyzc[0][i]}\t{xyzc[1][i]}\t{xyzc[2][i]}\t{xyzc[3][i]}\n".encode('utf-8').decode('utf-8'))
 
 
-def getDataFromFile(type, number, stepX=10, stepY=10, stepZ=10):
+def getDataFromFile(type, number, stepX=10, stepY=10, stepZ=10, path = "D:/VolSU/НИР/ScienceArticle/NotLinearCancerModel/NotLinearCancerModel/dataTumor/PredictData/PersonalPatients/"):
     """
     Get cancer data from file
 
@@ -25,16 +25,18 @@ def getDataFromFile(type, number, stepX=10, stepY=10, stepZ=10):
     stepX -- X-axis steep
     stepY -- Y-axis steep
     stepZ -- Z-axis steep
+    path -- path to directory file
 
     Return:
     Array[X-axis coordinates, Y-axis steep, Z-axis steep, degree of cancer damage (density)]
     """
 
+    path = path + str(type) + "/txt/" + str(number) + str(type) + ".txt"
     valuesX = []
     valuesY = []
     valuesZ = []
     valuesC = []
-    with open(f"D:/VolSU/НИР/ScienceArticle/NotLinearCancerModel/NotLinearCancerModel/dataTumor/PredictData/PersonalPatients/{type}/txt/{number}{type}.txt", "r") as file:
+    with open(path, "r") as file:
     # with open(f"../dataTumor/ModelData/personalPatients/poly3current/{type}/txt/{number}{type}.txt", "r") as file:
     # with open(f"'../dataTumor/ModelData/personalPatients/poly3current/Diameter/txt/1Diameter.txt'", "r") as file:
         i = 0
@@ -78,21 +80,30 @@ def getDataFromFile(type, number, stepX=10, stepY=10, stepZ=10):
     writeDataIntoFile(type, number, [valuesX2, valuesY2, valuesZ2, valuesC2])
     return [valuesX2, valuesY2, valuesZ2, valuesC2]
 
-def getTimeValueFromFile(type, number, stepX=10, stepY=10, stepZ=10):
+
+def getTimeValueFromFile(type, number, stepX=10, stepY=10, stepZ=10, path = "D:/VolSU/НИР/ScienceArticle/NotLinearCancerModel/NotLinearCancerModel/dataTumor/PredictData/PersonalPatients/"):
     """
     Get time, cancer-value (volume) from file
+
 
     Positional arguments:
     type -- data type (Volume or Diameter)
     number -- patient number
 
+    Keywords argiments:
+    stepX -- X-axis steep
+    stepY -- Y-axis steep
+    stepZ -- Z-axis steep
+    path -- path to directory file
+
     Return:
     Array[time-values, volumeCancer-values]
     """
 
+    path = path + str(type) + "/timeValue/txt/" + str(number) + str(type) + ".txt"
     valuesTime = []
     valuesCancer = []
-    with open(f"D:/VolSU/НИР/ScienceArticle/NotLinearCancerModel/NotLinearCancerModel/dataTumor/PredictData/PersonalPatients/{type}/timeValue/txt/{number}{type}.txt", "r") as file:
+    with open(path, "r") as file:
     # with open(f"../dataTumor/ModelData/personalPatients/poly3current/{type}/txt/{number}{type}.txt", "r") as file:
     # with open(f"'../dataTumor/ModelData/personalPatients/poly3current/Diameter/txt/1Diameter.txt'", "r") as file:
         for line in file.readlines():
@@ -111,7 +122,8 @@ def getTimeValueFromFile(type, number, stepX=10, stepY=10, stepZ=10):
         valuesTime2 = []
         valuesCancer2 = []
         for i in range(len(valuesTime)):
-            if valuesTime[i] not in valuesTime2 and valuesTime[i] < 125:
+            # if valuesTime[i] not in valuesTime2 and valuesTime[i] < 125:
+            if valuesTime[i] not in valuesTime2:
                 valuesTime2.append(valuesTime[i])
                 valuesCancer2.append(valuesCancer[i])
         # for i in range(len(valuesTime2)):        
@@ -121,6 +133,9 @@ def getTimeValueFromFile(type, number, stepX=10, stepY=10, stepZ=10):
         # if number == 1:
         #     for i in range(len(valuesTime2)):
         #         print(f"{valuesTime2[i]}\t{valuesCancer2[i]}")
+        # for i in range(len(valuesTime2)):
+        #     print(valuesTime2[i])
+        #     print(valuesCancer2[i])
     writeTimeValueIntoFile(type, number, [valuesTime2, valuesCancer2])
     return [valuesTime2, valuesCancer2]
 
