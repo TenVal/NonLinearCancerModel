2b0f17095b395200268d1cc813a97478d693c5f1
Ten Valeriya <kogaisvetlana@gmail.com>
2023-03-18 22:08:32 +0300

    create system diff eq and solve method RK

---
+++ NotLinearCancerModel/MethodDiffEquation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotLinearCancerModel
+{
+    class MethodDiffEquation
+    {
+    }
+
+    Array RK_2D(float t, float x0, float y0, float right, float h, float dx, float dy)
+    {
+        
+    }
+
+    Array RK_3D(float t, float x0, float y0, float z0, float right, float h, float dx, float dy, float dz)
+    {
+
+    }
+}
---
+++ NotLinearCancerModel/SystemOfEquation.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotLinearCancerModel
+{
+    class SystemOfEquation
+    {
+        private float l1;
+        private float b;
+        private float d;
+        private float e;
+        private float l3;
+        private float u;
+
+        public float dxR(float t, float x, float y, float z = 0)
+        {
+            float value = 0;
+            try
+            {
+                value =  (float)(-this.l1 * x * Math.Log(4 * Math.PI * (x * x * x) / (3 * y)) / 3);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine($"{e}");
+            }
+            return value;
+        }
+
+        public float dyR(float t, float x, float y, float z = 0)
+        {
+            float value = 0;
+            try
+            {
+                value = (float)(this.b * 4 * Math.PI * (x * x * x) / 3 - this.d * (Math.Pow(4 * Math.PI / 3, 2 / 3)) * (x * x) * y - this.e * z * y);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine($"{e}");
+            }
+            return value;
+        }
+
+        public float dx(float t, float x, float y, float z = 0)
+        {
+            float value = 0;
+            value = -this.l3 * z + this.u;
+            return value;
+        }
+
+        public float dy(float t, float x, float y, float z = 0)
+        {
+            float value = 0;
+            try
+            {
+                value = (float)(this.b * x - this.d * Math.Pow(x, 2 / 3) * y - this.e * z * y);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine($"{e}");
+            }
+            return value;
+        }
+
+        public float dz(float t, float x, float y, float z = 0)
+        {
+            float value = 0;
+            try
+            {
+                value = (float)(-this.l1 * x * Math.Log(x / y));
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine($"{e}");
+            }
+            return value;
+        }
+    }
+}
